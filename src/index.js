360337d423ee668c120f2498d33502cf276283e7
rock's ezweb <alikerock@gmail.com>
2024-06-07 17:26:51 +0900

    authService 추가

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
-import {firebase} from './firebase';
+import {authService} from './firebase';
+import { BrowserRouter } from 'react-router-dom';
 
-console.log(firebase);
+console.log(authService);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
