03481dc7e1bab86b9bc0a9d528c2d5e7c7ba4c4d
rock's ezweb <alikerock@gmail.com>
2024-06-10 13:16:55 +0900

    로그인여부 파악 함수 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
 import '../App.css';
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AppRouter from './Router';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 function App() {
   const[isLoggedIn, setIsloggedIn] = useState(false);
-  return <AppRouter isLoggedIn={isLoggedIn}/>;
+  const[init, setInit] = useState(false);
+  const auth = getAuth();
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsloggedIn(true);
+        //const uid = user.uid;
+
+      } else {
+        // User is signed out
+        setIsloggedIn(false);
+      }
+      setInit(true);
+    });
+  },[])
+
+  return (
+    <>
+    {init ?
+      <AppRouter isLoggedIn={isLoggedIn}/>
+      :"Intializing..."
+    }
+    </>
+  )
+  
 
 }
 
