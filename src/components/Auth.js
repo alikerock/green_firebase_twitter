e8f6771dd3e741a589dc7b1634f18a55e6c3e202
rock's ezweb <alikerock@gmail.com>
2024-06-10 13:16:41 +0900

    에러 출력 함수생성

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -6,6 +6,7 @@ const Auth = () => {
   const[email, setEmail] = useState('');
   const[password, setPassword] = useState('');
   const[newAccount, setNewAccount] = useState(true);
+  const[error, setError] = useState('');
   const auth = getAuth();
 
   const onChange = (e) =>{
@@ -36,19 +37,38 @@ const Auth = () => {
           const errorCode = error.code;
           const errorMessage = error.message;
           console.log(errorCode, errorMessage);
+          setError(errorMessage);
       });
     }else{
       //로그인
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log(user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        setError(errorMessage);
+      });
     }
   }
   console.log(email,password);
-
-  return(    
-    <form onSubmit={onSubmit}>
-      <input type="email" name="email" placeholder="Email 입력하세요" onChange={onChange} />
-      <input type="password" name="password" placeholder="비번을 입력하세요" onChange={onChange} />
-      <p><button>{newAccount ? '회원가입': '로그인'}</button></p>
-    </form>
+  //const toggleAccount = () => setNewAccount(!newAccount);
+  const toggleAccount = () => setNewAccount(prev=>!prev);
+  return( 
+    <>
+      <form onSubmit={onSubmit}>
+        <input type="email" name="email" placeholder="Email 입력하세요" onChange={onChange} />
+        <input type="password" name="password" placeholder="비번을 입력하세요" onChange={onChange} />
+        <p><button>{newAccount ? '회원가입': '로그인'}</button></p>
+        {error}
+      </form>
+      <hr/>
+      <button type="button" onClick={toggleAccount}>{newAccount ? "로그인으로 전환":"회원가입으로 전환"}</button>
+    </>   
   )
 }
 
