60c70df8c182ebf8e2e5a64f28660d9d4d5ad12f
rock's ezweb <alikerock@gmail.com>
2024-06-10 14:58:40 +0900

    nav 컴포넌트 추가

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import { Routes,Route } from "react-router-dom";
 import Auth from './Auth'
 import Home from '../routes/Home'
+import Nav from "./Nav";
 
 const AppRouter = ({isLoggedIn}) => {
   
   return(
     <Routes>
       {isLoggedIn ? 
-        <Route path='/' element={<Home/>}></Route> 
+        <>  
+          <Route path='/' element={<Nav/>}></Route> 
+          <Route path='/' element={<Home/>}></Route> 
+        </>
         :<Route path='/' element={<Auth/>}></Route>
       }
     </Routes>
