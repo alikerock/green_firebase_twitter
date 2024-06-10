b470484afd5e1180dea7dabc7544dabbba3b2919
rock's ezweb <alikerock@gmail.com>
2024-06-10 15:16:22 +0900

    로그인시 메뉴 출력

--- src/components/Router.js
+++ src/components/Router.js
@@ -3,19 +3,23 @@ import { Routes,Route } from "react-router-dom";
 import Auth from './Auth'
 import Home from '../routes/Home'
 import Nav from "./Nav";
+import Profile from "../routes/Profile";
 
 const AppRouter = ({isLoggedIn}) => {
   
   return(
-    <Routes>
-      {isLoggedIn ? 
-        <>  
-          <Route path='/' element={<Nav/>}></Route> 
-          <Route path='/' element={<Home/>}></Route> 
-        </>
-        :<Route path='/' element={<Auth/>}></Route>
-      }
-    </Routes>
+    <>
+      {isLoggedIn && <Nav/>} 
+      <Routes>
+        {isLoggedIn ? 
+          <>             
+            <Route path='/' element={<Home/>}></Route> 
+            <Route path='/profile' element={<Profile/>}></Route> 
+          </>
+          :<Route path='/' element={<Auth/>}></Route>
+        }
+      </Routes>
+    </>
   )
 }
 export default AppRouter;
