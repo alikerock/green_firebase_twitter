7836f36e0dc57a6254f5123cba0b846c4a3fd38e
rock's ezweb <alikerock@gmail.com>
2024-06-10 15:16:01 +0900

    메뉴 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Nav = ()=><nav>Navigation</nav>
+const Nav = ()=> (
+  <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/profile">Profile</Link></li>      
+    </ul>
+  </nav>
+);
 
 export default Nav;
