6bc2b4129fd93fa49e627f8d3626eaf585e2f126
rock's ezweb <alikerock@gmail.com>
2024-06-11 12:56:25 +0900

    수정 기능 추가

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,28 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import {db} from '../firebase';
-import { doc, deleteDoc } from "firebase/firestore";
+import { doc, deleteDoc, updateDoc} from "firebase/firestore";
 
 
 const Post = ({postObj, isOwener})=>{
-  console.log(postObj);
+  const [edit, setEdit]=useState(false);
+  const [newpost,setNewpost] = useState(postObj.content);
+
   const deletePost = async () =>{
     const question = window.confirm('정말 삭제할까요?');
     if(question){
       await deleteDoc(doc(db, "posts", postObj.id));
     }
   }
+  const toggleEditMode =()=>setEdit(prev=>!prev);
+  const onChange = (e) =>{
+    const{target:{value}} =e;
+    setNewpost(value);
+  }
+  const onSubmit = async (e) =>{
+    e.preventDefault();
+    const postRef = doc(db, "posts", postObj.id);
+
+    // Set the "capital" field of the city 'DC'
+    await updateDoc(postRef, {
+      content: newpost
+    });
+    setEdit(false);
+  }
   return(
     <li>
-      <h4>{postObj.content}</h4>
       {
-        isOwener && (
-        <> 
-          <button>수정</button>
-          <button onClick={deletePost}>삭제</button>
-        </>      
-        )      
+        edit ? (
+          <>
+          <form onSubmit={onSubmit}>
+              <input value={newpost} onChange={onChange} />
+              <button>업데이트</button>
+          </form>
+          <button onClick={toggleEditMode}>취소</button>
+          </>
+        ): (
+      <>
+        <h4>{postObj.content}</h4>
+        {
+          isOwener && (
+          <> 
+            <button onClick={toggleEditMode}>수정</button>
+            <button onClick={deletePost}>삭제</button>
+          </>      
+          )
+        }
+      </>      
+      )
       }      
-    </li>
+    </li>    
   )
 }
 
