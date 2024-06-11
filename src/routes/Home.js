e52eb15a5a532b3cb50843161b5c70e96ee78890
rock's ezweb <alikerock@gmail.com>
2024-06-11 10:20:34 +0900

    모든문서조회 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {db} from '../firebase';
-import { collection, addDoc, serverTimestamp} from "firebase/firestore"; 
+import { collection, addDoc, serverTimestamp, getDocs } from "firebase/firestore"; 
 
 const Home = () => {
   const[post, setPost] = useState('');
@@ -23,6 +23,16 @@ const Home = () => {
     }
     
   }
+  const getPosts = async () =>{
+    const querySnapshot = await getDocs(collection(db, "posts"));
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data().post);
+    });
+  }
+  useEffect(()=>{
+    getPosts();
+  },[]);
+
   return(
     <div>
       <form action="" onSubmit={onSubmit}>
