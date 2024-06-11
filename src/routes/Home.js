c89bb5bf494cd25ea503c78c3d2f5b5f8f5109b1
rock's ezweb <alikerock@gmail.com>
2024-06-11 09:58:57 +0900

    글입력 양식생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import {db} from '../firebase';
+import { collection, addDoc, serverTimestamp} from "firebase/firestore"; 
 
-const Home = () => <div>Home</div>
+const Home = () => {
+  const[post, setPost] = useState('');
+  const onChange = (e) =>{
+    //let value = e.target.value
+    const {target:{value}} = e;
+    setPost(value);
+  }
+  const onSubmit = async (e) =>{
+    e.preventDefault();
+    try {
+      const docRef = await addDoc(collection(db, "posts"), {
+        title: "Ada",
+        date:serverTimestamp()
+      });
+      setPost('');
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+    
+  }
+  return(
+    <div>
+      <form action="" onSubmit={onSubmit}>
+        <input type="text" value={post} placeholder="Write your twitt" onChange={onChange} />
+        <input type="submit" value="Add Post"/>
+      </form>
+    </div>
+  )
+}
 
 export default Home;
