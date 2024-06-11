9ea89581db966c4f4bd6261e54972848b6e5018b
rock's ezweb <alikerock@gmail.com>
2024-06-11 12:20:43 +0900

    삭제 버튼 추가

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-const Post = ({postObj})=>(
+const Post = ({postObj, isOwener})=>{
+  console.log(postObj);
+  const deletePost = () =>{
+    const question = window.confirm('정말 삭제할까요?');
+    if(question){
+
+    }
+  }
+  return(
     <li>
-      <h4>{postObj}</h4>
-      <button>수정</button>
-      <button>삭제</button>
+      <h4>{postObj.content}</h4>
+      {
+        isOwener && (
+        <> 
+          <button>수정</button>
+          <button onClick={deletePost}>삭제</button>
+        </>      
+        )      
+      }      
     </li>
-  );
+  )
+}
 
 export default Post;
