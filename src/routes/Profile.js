f71322cc9a52e4c8754855540f38341caf72bdb1
rock's ezweb <alikerock@gmail.com>
2024-06-12 13:16:19 +0900

    profile updata 완료

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { getAuth, signOut } from "firebase/auth";
+import React,{useEffect, useState} from "react";
+import { getAuth, signOut, updateProfile  } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { getStorage, ref, uploadBytes, getDownloadURL} from "firebase/storage";
 
 const Profile = () => {
+  const[profileImg, setProfileImg] = useState(`${process.env.PUBLIC_URL}/profile_icon.svg`);
   const auth = getAuth();
   const navigate = useNavigate();
+  console.log(auth);
 
   const logOut = () => signOut(auth).then(() => {
       // Sign-out successful.
@@ -14,14 +16,37 @@ const Profile = () => {
       // An error happened.
     });
 
+  const updateLogo = async (e)=>{
+    const {target:{files}} = e;
+    const file = files[0];
+    const storage = getStorage();
+    const profileLogoRef = ref(storage, `profile/${auth.currentUser.uid}`);
+    const result = await uploadBytes(profileLogoRef, file);
+    console.log(result);
+    const profileUrl = await getDownloadURL(result.ref);
+    console.log(profileUrl);
+    setProfileImg(profileUrl);
+
+    await updateProfile(auth.currentUser, {
+      photoURL:profileUrl
+    })
+
+  }  
+ useEffect(()=>{
+  auth.currentUser.photoURL.includes('firebase') && setProfileImg(auth.currentUser.photoURL);
+ },[]);
+
   return(
     <>
     <div className="profile">
-      <img src={`${process.env.PUBLIC_URL}/profile_icon.svg`} alt=""/>
-      <input type="file" className="hidden" id="profile" accept="image/*" />
-      <label for="profile">프로필 수정</label>
+      <img src={profileImg} alt=""/>      
+      <input type="file" className="hidden" id="profile" accept="image/*" onChange={updateLogo} />      
     </div>
+    <h3>{auth.currentUser.displayName}</h3>
+    <label htmlFor="profile">프로필 수정</label>
     <button onClick={logOut}>Log out</button>
+    <hr/>
+    <h4>My post</h4>
     </>
   )
 }
